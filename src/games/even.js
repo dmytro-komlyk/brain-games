19033fa86a04089e90d2ac515e680ff1c83d0503
Komlyk Dmytro <dima.komlyk@icloud.com>
2020-03-27 13:41:09 +0100

    fixes v3

--- src/games/even.js
+++ src/games/even.js
@@ -4,10 +4,9 @@ import getRandom from '../math-utilities.js';
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (num) => num % 2 === 0;
 
-
 const gameData = () => {
   const question = getRandom(1, 100);
-  const answer = (isEven(question)) ? 'yes' : 'no';
+  const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
